5e9e34174c5df0573742421602b54a63a3266099
Sam Orme <ormesam@hotmail.com>
2022-04-21 11:55:12 +0100

    Add invert decorator

---
+++ src/Assets/Splatter.AI/Runtime/InvertDecorator.cs
@@ -0,0 +1,19 @@
+﻿namespace Splatter.AI {
+    public class InvertDecorator : Decorator {
+        public InvertDecorator(BehaviourTree tree) : base("Inverter", tree) {
+        }
+
+        protected override NodeResult ExecuteNode() {
+            var result = Child.Execute();
+
+            switch (result) {
+                case NodeResult.Failure:
+                    return NodeResult.Success;
+                case NodeResult.Success:
+                    return NodeResult.Failure;
+                default:
+                    return NodeResult.Running;
+            }
+        }
+    }
+}
--- src/Assets/Splatter.AI/Tests/DecoratorTests.cs
+++ src/Assets/Splatter.AI/Tests/DecoratorTests.cs
@@ -28,5 +28,29 @@ namespace Splatter.AI.Tests {
 
             Assert.AreEqual(NodeResult.Running, decorator.Execute());
         }
+
+        [Test]
+        public void Invert_Running() {
+            var decorator = new InvertDecorator(Tree);
+            decorator.Child = CreateRunningNode();
+
+            Assert.AreEqual(NodeResult.Running, decorator.Execute());
+        }
+
+        [Test]
+        public void Invert_Success() {
+            var decorator = new InvertDecorator(Tree);
+            decorator.Child = CreateFailureNode();
+
+            Assert.AreEqual(NodeResult.Success, decorator.Execute());
+        }
+
+        [Test]
+        public void Invert_Failure() {
+            var decorator = new InvertDecorator(Tree);
+            decorator.Child = CreateSuccessNode();
+
+            Assert.AreEqual(NodeResult.Failure, decorator.Execute());
+        }
     }
 }
